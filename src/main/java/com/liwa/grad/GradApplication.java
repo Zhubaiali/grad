2911a80b8d63db73f0a5f37d81fcfd37fd70c79e
liwa <hogwartsghls@gmail.com>
2023-05-25 17:01:34 +0800

    项目初始化

---
+++ src/main/java/com/liwa/grad/GradApplication.java
@@ -0,0 +1,14 @@
+package com.liwa.grad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradApplication.class, args);
+        System.out.println("Hello Grad");
+    }
+
+}
---
+++ src/main/java/com/liwa/grad/annotation/LogExecutionTime.java
@@ -0,0 +1,4 @@
+package com.liwa.grad.annotation;
+
+public class LogExecutionTime {
+}
